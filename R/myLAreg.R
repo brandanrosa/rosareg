7f83e13407ea807574024a0fb1efea7b91d43eee
Brandan Rosa <brandanrosa22@gmai.com>
2024-06-26 14:51:10 -0500

    Initial

---
+++ R/myLAreg.R
@@ -0,0 +1,37 @@
+#' myLAreg
+#'
+#' A function which takes an `lm()` object and produces a list of fun and interesting values which Regression covets
+#'
+#' @param obj an `lm()` object
+#'
+#' @return a named list with the following: model summary, n (num observations), k (num beta parameters), beta estimates, RSS, s-squared, s, and the Design & Response Matrices
+#' @export
+#'
+#' @examples \dontrun{myLAreg(ylm)}
+myLAreg <- function(obj) { # obj = lm() object
+
+  # summary
+  sm <- summary(obj)
+
+  # data frame
+  dat <- stats::model.frame(obj)
+
+  # n and k
+  n <- dim(dat)[1]
+  k <- dim(dat)[2]
+
+  # matrices
+  X <- stats::model.matrix(obj)
+  Y <- dat[, 1]
+  Y <- as.matrix(Y)
+
+  # beta estimates
+  betahat <- solve(t(X) %*% X) %*% t(X) %*% Y
+
+  # RSS
+  RSS <- t(Y) %*% Y - t(betahat) %*% t(X) %*% Y
+
+  # ssq & s
+  ssq <- RSS / (n - (k + 1))
+  s <- sqrt(ssq)
+}
